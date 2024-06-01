d4937abe8fee68166544fd0cca2514dfa99b7d62
Bhaumik Singh <Bhaumiksingh2000@gmail.com>
2024-06-01 21:58:48 +0530

    Add Next.js and PostCSS configuration files, global styles, and ignore files

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="font-sans items-center justify-items-center min-h-screen">
+      {/* <header className="bg-gray-800 text-white p-4">
+        <h1 className="text-2xl">Welcome to your new app</h1>
+      </header> */}
+    </div>
+  );
+}
