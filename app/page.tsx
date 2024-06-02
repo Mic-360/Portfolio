1a30fa7a9818c5b20d505d19403a9b5f6c678ebc
Bhaumik Singh <Bhaumiksingh2000@gmail.com>
2024-06-02 17:12:12 +0530

    Update responsive banner section
    Signed-off-by: Bhaumik Singh <Bhaumiksingh2000@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -17,9 +17,11 @@ export default function Home() {
       <header className="space-y-4">
         <div className="mt-14 flex justify-between items-center">
           <BoxReveal boxColor={"#a6a6a6"} duration={0.5}>
-            <h1 className="font-mono text-8xl font-semibold">Full-stack</h1>
+            <h1 className="font-mono text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-semibold">
+              Full-stack
+            </h1>
           </BoxReveal>
-          <div className="flex gap-x-2 items-center justify-center pr-8 animate-fade-in">
+          <div className="hidden md:flex gap-x-2 items-center justify-center pr-10 animate-fade-in">
             <div className="px-20 font-normal bg-foreground rounded-full py-1.5 text-background italic">
               Projects
             </div>
@@ -31,7 +33,7 @@ export default function Home() {
             </Link>
           </div>
         </div>
-        <div className="flex justify-between px-2">
+        <div className="flex flex-col-reverse md:flex-row gap-4 justify-between text-start lg:items-center px-2 py-4 lg:py-0">
           <BoxReveal boxColor={"#a6a6a6"} duration={0.5}>
             <p className="w-72 text-shade">
               My goal is to{" "}
@@ -46,8 +48,21 @@ export default function Home() {
             </p>
           </BoxReveal>
           <BoxReveal boxColor={"#a6a6a6"} duration={0.5}>
-            <h1 className="font-mono text-8xl font-semibold">Developer</h1>
+            <h1 className="font-mono text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-semibold pl-16 lg:pl-0">
+              Developer
+            </h1>
           </BoxReveal>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center justify-start animate-fade-in">
+          <div className="px-20 font-normal bg-foreground rounded-full py-1.5 text-background italic">
+            Projects
+          </div>
+          <Link
+            href="/about"
+            className="bg-foreground p-2 rounded-full text-background"
+          >
+            <Icons.right className="h-4 w-4" />
+          </Link>
         </div>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:flex px-8 py-8 items-start md:items-center justify-center gap-2.5">
           {FooterButtons.map(
@@ -66,14 +81,11 @@ export default function Home() {
         </div>
       </header>
       {/* Carousel  */}
-      <section className="px-20 py-10">
+      <section className="px-8 py-10 mb-20">
         <Carousel>
           <CarouselContent>
             {Array.from({ length: 6 }).map((_, index) => (
-              <CarouselItem
-                key={index}
-                className="md:basis-1/2 lg:basis-3/6 space-x-4"
-              >
+              <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
                 <div
                   className="flex flex-row-reverse border-2 w-full h-60 rounded-3xl"
                   style={{
@@ -82,17 +94,21 @@ export default function Home() {
                     backgroundSize: "cover",
                   }}
                 >
-                  <div className="p-8 w-2/3 rounded-e-3xl backdrop-blur-md space-y-4 backdrop-brightness-50">
-                    <h1 className="text-foreground font-semibold text-xl">
-                      Project Name
-                    </h1>
-                    <p className="text-foreground">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Quam, quas?
-                    </p>
+                  <div className="flex flex-col items-start justify-between p-4 w-2/3 rounded-e-3xl backdrop-blur-md space-y-4 backdrop-brightness-50">
+                    <div className="space-y-4">
+                      <h1 className="text-foreground font-semibold text-xl font-mono">
+                        Project Name
+                      </h1>
+                      <p className="text-foreground text-xs">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Quas necessitatibus, ad, accusantium fugit vero
+                        qui enim nobis voluptates, itaque pariatur doloribus
+                        commodi.
+                      </p>
+                    </div>
                     <div className="flex gap-x-2 items-center justify-center pr-8">
-                      <div className="px-8 font-normal bg-white rounded-full py-1.5 text-background italic text-sm">
-                        Read More ...
+                      <div className="px-4 font-normal bg-white rounded-full py-1.5 text-background italic text-xs">
+                        Read More...
                       </div>
                       <Link
                         href="/about"
@@ -106,8 +122,8 @@ export default function Home() {
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
+          <CarouselPrevious className="backdrop-blur-lg bg-background" />
+          <CarouselNext className="backdrop-blur-lg bg-background" />
         </Carousel>
       </section>
     </div>
