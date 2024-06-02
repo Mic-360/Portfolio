e430da83519580065ad8a2962ee633787aad3274
Bhaumik Singh <Bhaumiksingh2000@gmail.com>
2024-06-02 07:02:39 +0530

    Update responsive header, footer and icons
    Signed-off-by: Bhaumik Singh <Bhaumiksingh2000@gmail.com>

--- components/Outlet.tsx
+++ components/Outlet.tsx
@@ -3,29 +3,49 @@ import { FooterButtons } from "@/lib/constants";
 import Link from "next/link";
 import Meteors from "./magicui/meteors";
 import ShineBorder from "./magicui/shine-border";
+import { Icons } from "@/lib/icons";
 
 interface HeaderProps {}
 
 const Header: FC<HeaderProps> = () => {
   return (
-    <nav className="sticky top-1 flex items-center justify-between py-2 font-sans overflow-hidden backdrop-blur-xl rounded-3xl px-10">
+    <nav className="sticky top-1 flex items-center justify-between py-2 font-sans overflow-hidden backdrop-blur-xl rounded-3xl px-2 sm:px-10">
       <Meteors number={30} />
-      <h1 className="text-2xl w-24 cursor-none font-medium">Bhaumik Singh</h1>
-      <ul className="flex gap-x-10 cursor-pointer text-foreground transition-all duration-300 ease-in-out">
-        <li className="py-1 px-4 rounded-3xl capitalize font-sans font-light hover:bg-accent hover:text-shade transition-all duration-300 ease-in-out">
-          <Link href="#about">About</Link>
+      <h1 className="text-lg md:text-3xl w-full md:w-24 cursor-none font-medium">
+        Bhaumik Singh
+      </h1>
+      <ul className="md:flex gap-x-4 cursor-pointer text-foreground transition-all duration-300 ease-in-out hidden">
+        <li className="py-1 px-4 rounded-3xl capitalize font-sans font-light hover:bg-accent hover:text-shade hover:underline underline-offset-2 transition-all duration-300 ease-in-out">
+          <Link href="#about">
+            About
+            <span className="font-mono font-normal text-xs pl-1">\.. </span>
+          </Link>
         </li>
-        <li className="py-1 px-4 rounded-3xl capitalize font-sans font-light hover:bg-accent hover:text-shade transition-all duration-300 ease-in-out">
-          <Link href="#project">Project</Link>
+        <li className="py-1 px-4 rounded-3xl capitalize font-sans font-light hover:bg-accent hover:text-shade hover:underline underline-offset-2 transition-all duration-300 ease-in-out">
+          <Link href="#project">
+            Project
+            <span className="font-mono font-normal text-xs pl-1">\.. </span>
+          </Link>
         </li>
-        <li className="py-1 px-4 rounded-3xl capitalize font-sans font-light hover:bg-accent hover:text-shade transition-all duration-300 ease-in-out">
-          <Link href="#article">Article</Link>
+        <li className="py-1 px-4 rounded-3xl capitalize font-sans font-light hover:bg-accent hover:text-shade hover:underline underline-offset-2 transition-all duration-300 ease-in-out">
+          <Link href="#article">
+            Article
+            <span className="font-mono font-normal text-xs pl-1">\.. </span>
+          </Link>
         </li>
-        <li className="py-1 px-4 rounded-3xl capitalize font-sans font-light hover:bg-accent hover:text-shade transition-all duration-300 ease-in-out">
-          <Link href="#contact">Contact</Link>
+        <li className="py-1 px-4 rounded-3xl capitalize font-sans font-light hover:bg-accent hover:text-shade hover:underline underline-offset-2 transition-all duration-300 ease-in-out">
+          <Link href="#contact">
+            Contact
+            <span className="font-mono font-normal text-xs pl-1">\.. </span>
+          </Link>
         </li>
       </ul>
-      <div className="cursor-not-allowed">EN</div>
+      <Link
+        href="https://github.com/Mic-360/portfolio"
+        className="cursor-pointer"
+      >
+        <Icons.github className="h-6 w-6" />
+      </Link>
     </nav>
   );
 };
@@ -35,39 +55,39 @@ interface FooterProps {}
 const Footer: FC<FooterProps> = () => {
   return (
     <footer>
-      <div className="flex gap-x-8 items-center">
-        <div className="font-mono font-semibold space-y-8 pl-10 pt-20">
+      <div className="flex flex-col-reverse md:flex-row gap-8">
+        <div className="font-mono font-semibold space-y-8 pl-10 ">
           <div>
-            <h2 className="text-8xl">Bhaumik</h2>
+            <h2 className="text-6xl sm:text-7xl md:text-8xl lg:text-9xl">Bhaumik</h2>
           </div>
-          <div className="flex gap-x-32 items-center">
-            <p className="font-sans font-normal text-shade tracking-wider">
+          <div className="flex gap-x-10 md:gap-x-20 lg:gap-x-32 items-center">
+            <p className="font-sans font-normal text-shade tracking-wider text-sm md:text-lg">
               Full-stack
               <br />
               developer
             </p>
-            <h2 className="text-8xl">Singh</h2>
+            <h2 className="text-6xl sm:text-7xl md:text-8xl lg:text-9xl">Singh</h2>
           </div>
         </div>
-        <div className="px-10 space-y-8 w-full font-sans">
-          <h2 className="text-xl font-medium font-mono px-10">
+        <div className="text-left flex flex-col items-start md:items-center space-y-8 w-full font-sans px-8 lg:items-end">
+          <h2 className="text-xl font-medium font-mono w-72">
             .../Contacts...
           </h2>
-          <ul className="flex justify-around w-full cursor-pointer transition-all duration-300 ease-in-out">
+          <ul className="flex gap-x-8 cursor-pointer transition-all duration-300 ease-in-out">
             <li className="text-shade hover:text-accent hover:underline underline-offset-2">
-              Main
+              Connect
             </li>
             <li className="text-shade hover:text-accent hover:underline underline-offset-2">
               About
             </li>
             <li className="text-shade hover:text-accent hover:underline underline-offset-2">
-              Project
+              Projects
             </li>
             <li className="text-shade hover:text-accent hover:underline underline-offset-2">
               Articles
             </li>
           </ul>
-          <div className="px-10">
+          <div className="w-72">
             <div className="border border-accent rounded-3xl p-4 space-y-2 text-shade font-mono relative">
               <ShineBorder
                 borderRadius={22}
@@ -75,14 +95,14 @@ const Footer: FC<FooterProps> = () => {
               >
                 <p className="text-xl mb-2 font-sans">Site</p>
                 <p className="text-sm">Handcrafted by ME /</p>
-                <p className="text-sm">Designed by ME /</p>
+                <p className="text-sm">Designed by v0 /</p>
                 <p className="text-sm">Powered by Next Js</p>
               </ShineBorder>
             </div>
           </div>
         </div>
       </div>
-      <div className="flex my-10 items-center justify-center gap-x-2">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:flex my-10 px-8 items-start md:items-center justify-center gap-2">
         {FooterButtons.map((button, idx) => (
           <Link
             key={idx}
--- lib/constants.tsx
+++ lib/constants.tsx
@@ -3,33 +3,33 @@ import { Icons } from "./icons";
 
 export const FooterButtons: FooterButton[] = [
   {
+    link: "mailto:bhaumiksingh2000@gmail.com",
+    name: "e-mail",
+    icon: <Icons.email className="w-[16px] h-[16px]" />,
+  },
+  {
     link: "https://www.github.com/Mic-360/",
     name: "github",
-    icon: <Icons.github className="w-4 h-4" />,
+    icon: <Icons.github className="w-[16px] h-[16px]" />,
   },
   {
     link: "https://www.linkedin.com",
     name: "linkedin",
-    icon: <Icons.linkedin className="w-4 h-4" />,
-  },
-  {
-    link: "mailto:bhaumiksingh2000@gmail.com",
-    name: "e-mail",
-    icon: <Icons.email className="w-4 h-4" />,
+    icon: <Icons.linkedin className="w-[16px] h-[16px]" />,
   },
   {
     link: "https://t.me/bhaumic_kun",
     name: "telegram",
-    icon: <Icons.telegram className="w-4 h-4" />,
+    icon: <Icons.telegram className="w-[16px] h-[16px]" />,
   },
   {
     link: "https://www.facebook.com/Miczx.in",
     name: "facebook",
-    icon: <Icons.facebook className="w-4 h-4" />,
+    icon: <Icons.facebook className="w-[16px] h-[16px]" />,
   },
   {
     link: "https://www.instagram.com/just.a.fat.rat/",
     name: "instagram",
-    icon: <Icons.instagram className="w-4 h-4" />,
+    icon: <Icons.instagram className="w-[16px] h-[16px]" />,
   },
 ];
